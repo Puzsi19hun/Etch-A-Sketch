733de487376c7a1e8165607e6af4428650998e3f
Puzsi <puzsi19@gmail.com>
2024-02-28 16:11:41 +0100

    Done

--- script.js
+++ script.js
@@ -1,37 +1,73 @@
-const container = document.querySelector(".container")
-container.style.width = "100%"
-container.style.height = "100%"
-sor = 0
-for (let i = 0; i < 64; i++) {
-    for (j = 0; j < 64; j++) {
+const draw = document.querySelector(".draw");
+const sliderVal = document.querySelector("#sliderVal");
+const slider = document.querySelector(".slider");
+var mode = "Draw";
+var darken = document.querySelector(".progdark")
+var squareSize = 8;
 
-        let div = document.createElement("div")
-        var breakcol = document.createElement("div")
+createGrid(squareSize)
 
-        if (sor == 644) {
-            breakcol.style.display = "flex"
-            breakcol.style.flexBasis = "100%"
-            breakcol.style.width = "0"
-            container.appendChild(breakcol)
-            sor = 0
-        }
+function slide() {
+    sliderVal.innerText = slider.value;
+}
 
-        div.style.border = "1px solid black"
-        div.style.display = "inline-flex"
-        div.style.width = "5vh"
-        div.style.height = "5vh"
-        div.style.marginRight = "5px"
-        div.style.marginBottom = "5px"
-        div.addEventListener("mouseover", function () {
-            div.style.backgroundColor = "black"
-        })
-        container.appendChild(div)
-
-
-        sor = sor + 1
+function progdarkon() {
+    if (darken.checked) {
+        progressiveDark = true;
     }
-
+    else {
+        progressiveDark = false;
+    }
 
 }
 
+
+
+function drawing() {
+    mode = "Draw";
+}
+
+function clear() {
+    reset();
+}
+
+function erase() {
+    mode = "Erase";
+}
+
+
+function createDiv(size) {
+    const div = document.createElement("div");
+    div.style.border = "0.5px black solid";
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.addEventListener('mouseover', colorDiv)
+    return div;
+}
+
+function colorDiv()
+{
+    if(mode == "Draw"){this.style.backgroundColor = document.querySelector(".color").value;}
+    else{this.style.backgroundColor = 'white';}
+}
+
+
+function changeGrid() {
+    squareSize = slider.value;
+    reset();
+    createGrid(squareSize);
+}
+
+function createGrid(Gridsize) {
+    for (let i = 0; i < Gridsize; i++) {
+        for (let j = 0; j < Gridsize; j++) {
+            draw.appendChild(createDiv(draw.clientWidth / Gridsize));
+        }
+
+    }
+}
+
+function reset() {
+    draw.replaceChildren()
+}
 
